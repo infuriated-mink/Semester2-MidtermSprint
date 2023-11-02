259464c722a9f38de5e8abfdb1166fac3494f9cb
Vanessa Rice <vanessa.rice@keyin.com>
2023-11-01 22:49:05 -0230

    Update RecipeSearch.js

--- react-login/src/components/RecipeSearch.js
+++ react-login/src/components/RecipeSearch.js
@@ -7,14 +7,35 @@ function RecipeSearch({ onSearch }) {
     onSearch(searchQuery);
   };
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+  const handleClearSearch = () => {
+=======
+
+  const clearSearch = () => {
+>>>>>>> Stashed changes
+    setSearchQuery('');
+    onSearch('');
+  };
+
+>>>>>>> Stashed changes
   return (
     <div className="recipe-search-container">
       <input
         type="text"
         placeholder="Search for recipes..."
         value={searchQuery}
+<<<<<<< Updated upstream
         onChange={(e) => setSearchQuery(e.target.value)} 
         className="rounded-search-bar"
+=======
+        onChange={(e) => setSearchQuery(e.target.value)}
+<<<<<<< Updated upstream
+=======
+        className="rounded-search-bar"
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       />
       <button onClick={handleSearch} className="rounded-search-button">
         Search
