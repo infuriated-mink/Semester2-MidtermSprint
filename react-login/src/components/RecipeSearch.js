7fa94579268c75351e0cbf9a39b22e33bec1bcbe
bhavik from zorin-os <15388512+bhavik-knight@users.noreply.github.com>
2023-11-01 22:46:27 -0300

    fixed the broken push by Vanessa on main

--- react-login/src/components/RecipeSearch.js
+++ react-login/src/components/RecipeSearch.js
@@ -12,35 +12,13 @@ function RecipeSearch({ onSearch }) {
     onSearch('');
   };
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-  const handleClearSearch = () => {
-=======
-
-  const clearSearch = () => {
->>>>>>> Stashed changes
-    setSearchQuery('');
-    onSearch('');
-  };
-
->>>>>>> Stashed changes
   return (
     <div>
       <input
         type="text"
         placeholder="Search for recipes..."
         value={searchQuery}
-<<<<<<< Updated upstream
-        onChange={(e) => setSearchQuery(e.target.value)} 
-        className="rounded-search-bar"
-=======
         onChange={(e) => setSearchQuery(e.target.value)}
-<<<<<<< Updated upstream
-=======
-        className="rounded-search-bar"
->>>>>>> Stashed changes
->>>>>>> Stashed changes
       />
       <button onClick={handleSearch}>Search</button>
       {
