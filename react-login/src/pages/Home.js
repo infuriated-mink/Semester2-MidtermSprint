7044ea050b28d10eada46262dd6fcb8a55aee902
bhavik from zorin-os <15388512+bhavik-knight@users.noreply.github.com>
2023-11-05 16:21:37 -0400

    logo

--- react-login/src/pages/Home.js
+++ react-login/src/pages/Home.js
@@ -49,10 +49,12 @@ function Home({ recipes: propRecipes }) {
       <div className="mainbox">
         {/* Header */}
         <div className="header">
-          <div className="horizontal-bar"></div>
-          <div className="logo"></div>
+          {/* <div className="horizontal-bar"></div> */}
+          <div className="logo-container">
+            <img src="/media/logo.png" alt="logo" />
+          </div>
           <div className="slogan">Build better recipes, together!</div>
-          <div className="horizontal-bar2"></div>
+          {/* <div className="horizontal-bar2"></div> */}
           <RecipeSearch onSearch={setSearchQuery} />
         </div>
         <div className="top-right">
